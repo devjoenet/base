fd19fd209e654dcfc18dbda581c389600080f4d5
Joseph Harris <devjoenet@gmail.com>
2025-12-05 15:32:42 -0500

    Add accordion UI component

---
+++ resources/js/components/ui/accordion/Accordion.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { cn } from "@/lib/utils";
+  import type { HTMLAttributes } from "vue";
+
+  const props = defineProps<{
+    class?: HTMLAttributes["class"];
+  }>();
+</script>
+
+<template>
+  <dl :class="cn('divide-y divide-gray-900/10 dark:divide-white/10', props.class)">
+    <slot />
+  </dl>
+</template>
---
+++ resources/js/components/ui/accordion/index.ts
@@ -0,0 +1,2 @@
+export { default as Accordion } from "@/components/ui/accordion/Accordion.vue";
+export { default as AccordionItem } from "@/components/ui/accordion/AccordionItem.vue";
