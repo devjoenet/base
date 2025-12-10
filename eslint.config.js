33109f96a3303964fb814eee78ac6dcc8496bba2
Joey Harris <devjoenet@gmail.com>
2025-12-10 11:28:26 -0500

    Updated and applied formatting rules and refactor rules in pint, prettier, eslint and rector.

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,20 @@
-import prettier from 'eslint-config-prettier/flat';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import prettier from 'eslint-config-prettier';
 import vue from 'eslint-plugin-vue';
 
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
-
 export default defineConfigWithVueTs(
-    vue.configs['flat/essential'],
-    vueTsConfigs.recommended,
-    {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'resources/js/components/ui/*'],
+  vue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  {
+    ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/script-setup-uses-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
-    {
-        rules: {
-            'vue/multi-word-component-names': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-        },
-    },
-    prettier,
+  },
+  prettier,
 );
